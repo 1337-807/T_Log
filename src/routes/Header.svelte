c5d29c49f1e70f84b57eea08bba23f12f80382da
1337-807 <1337.a.bot@gmail.com>
2023-09-22 04:24:37 +0200

    fix: header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -10,7 +10,7 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === `/${base}` ? 'page' : undefined}>
+			<li aria-current={$page.url.pathname === `${base}` ? 'page' : undefined}>
 				<a href="{base}/">Home</a>
 			</li>
 			<li aria-current={$page.url.pathname === `${base}/giftdashboard` ? 'page' : undefined}>
